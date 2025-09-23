94ff436f6a08f0cc9204b59b3c248968b072c8cf
ntapire <digantarajbaral@gmail.com>
2025-09-23 10:14:20 +0545

    This is the very first commit containg a basic setup for the project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const moongose = require("mongoose");
+const port = 3000;
+mongoose.connect('mongodb://127.0.0.1:27017/wanderLust');
+
+app.listen(port,()=>{
+    console.log("Server is listening on port",port);
+})
+
+app.get("/",(req,res)=>{
+    res.send("Hi I am root")
+})
+
+Main().then((result)=>{
+    console.log("Connection success");
+}).catch((err)=>{
+ console.log("some error happened")
+})
